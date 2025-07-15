4a31d9b74fc20e578c285b527f21d4e5fdade096
KigoJomo <kigojomo@gmail.com>
2025-07-15 18:43:44 +0300

    refactor: Remove ChatUi component and its usage from ChatPage

--- app/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatUi from './components/ChatUi';
-
-export default function ChatPage() {
-  return (
-    <section className="flex-1 overflow-y-auto relative hide-scrollbar flex flex-col items-center">
-      <ChatUi />
-    </section>
-  );
-}
